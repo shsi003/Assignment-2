0a6864bae2c07ea072a6b66ddc33aa85ae9319c5
Shakinthan Sivalingam <144006288+shsi003@users.noreply.github.com>
2024-08-13 00:31:35 +0200

    declaring product class

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
+	const products = [];
+
+	const productRegistry = document.querySelector(".Product-registry");
+	const productName = document.querySelector(".Product-name");
+	const productID = document.querySelector(".Product-ID");
+	const manufacturer = document.querySelector(".manufacturer");
+	const expirationDate = document.querySelector(".Expiration-date");
+	const quantity = document.querySelector(".Quantity");
+	const productListUl = document.querySelector(".product-list-ul");
+
+	productRegistry.addEventListener("submit", (e) => {
+		e.preventDefault();
+		
+		Product.addProduct(newProduct);
+		productRegistry.reset();
+		console.log(newProduct);
+		console.log(products);
+	})
+
+	class Product {
+		constructor(productName, productID, manufacturer, expirationDate, quanity){
+			this.productName = productName;
+			this.productID = productID;
+			this.manufacturer = manufacturer;
+			this.expiraitonDate = expirationDate;
+			this.quantity = qunatity;
+		}
+
+		static deleteProduct(id, productsArray){
+			const index = productsArray.findIndex(product => product.ID.toString() === id.toString());
+			if(index !== -1){
+				productsArray.splice(index, 1);
+			}
+		}
+	}
+
 	
 });
